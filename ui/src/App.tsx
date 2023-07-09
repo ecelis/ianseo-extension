df82f8b2588b5f4d5102c26bfa122fcb0c9dcc0c
Ernesto Celis <ernesto@celisdelafuente.net>
2023-07-08 18:07:15 -0600

    feat(extension): prepare to publish in market [ID-27]

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -4,6 +4,7 @@ import Link from '@mui/material/Link';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
 
+
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
 const client = createDockerDesktopClient();
@@ -14,27 +15,33 @@ function useDockerDesktopClient() {
 
 export function App() {
   const [response, setResponse] = React.useState<string>();
+  const [ianseoUrl, setIanseoUrl] = React.useState<string>('http://localhost:8080');
   const ddClient = useDockerDesktopClient();
 
   return (
     <>
-      <Typography variant="h3">Arqueria Desktop Extension</Typography>
+      <Typography variant="h3"><img src="arqueria.svg" width="45" height="45" />{' '}
+        Arqueria Desktop Extension</Typography>
       <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-        {`The extension allows Ianseo archery tournaments management software
-        to run in a docker desktop runtime environment.
+        <p>The extension allows Ianseo archery tournaments management software
+        to run in a docker desktop runtime environment.</p>
 
-        The goal is to present a simple and unified way to run the software
-        cross-platform.
+        <p>The goal is to present a simple and unified way to run the software
+        cross-platform.</p>
 
-        Go ahead and endjoy the shooting! 🎯 Your Ianseo is at ➡️ ${<Link href="http://localhost:8080">Ianseo</Link>} ⬅️
+        <p>Go ahead and endjoy the shooting! 🎯 Your Ianseo is at ➡️ 
+        <Link href={ianseoUrl}>{ianseoUrl}</Link> ⬅️ </p>
 
-        This work builds on top of other people work, the open source community
-        but specially the ${<Link href="https://ianseo.net">Ianseo Team</Link>}
+        <p>It builds on top of other people work, the open source community
+        and specially the <Link href="https://ianseo.net">Ianseo Team</Link></p>
 
-        ${<Link href="https://ianseo.net/Releases.php">Ianseoo Documentation</Link>}
+        <p><Link href="https://ianseo.net/Releases.php">Ianseo Documentation</Link></p>
 
-        Arqueria desktop extension ${<Link href="https://ianseo.arqueria.pro">website</Link>}`}
+        <p>Arqueria desktop extension <Link href="https://ianseo.arqueria.pro">website</Link></p>
       </Typography>
+      <iframe width="100%" height="400px"
+      src={ianseoUrl}
+      sandbox="allow-scripts allow-modal"></iframe>
     </>
   );
 }
